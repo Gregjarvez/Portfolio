0f5211a5141f3bee6084f96983b1b002eefaf1e9
GREGORY ASSASIE <gregjarvez@gmail.com>
2017-06-21 10:45:23 +0100

    animations complete

--- app/temp/scripts/vendor.js
+++ app/temp/scripts/vendor.js
@@ -63,12 +63,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ 	return __webpack_require__(__webpack_require__.s = 121);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */,
-/* 1 */
+/******/ ({
+
+/***/ 119:
 /***/ (function(module, exports) {
 
 /* -----------------------------------------------
@@ -1614,15 +1614,14 @@ window.particlesJS.load = function(tag_id, path_config_json, callback){
 };
 
 /***/ }),
-/* 2 */,
-/* 3 */,
-/* 4 */
+
+/***/ 121:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _particles = __webpack_require__(1);
+var _particles = __webpack_require__(119);
 
 var _particles2 = _interopRequireDefault(_particles);
 
@@ -1632,4 +1631,5 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 particlesJS.load('particles--bg', '../../assets/particles.json', function () {});
 
 /***/ })
-/******/ ]);
+
+/******/ });
