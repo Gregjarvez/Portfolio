e82191661cf29ddb6c451be89b7cd9dc687afdac
GREGORY ASSASIE <gregjarvez@gmail.com>
2017-06-25 12:31:57 +0100

    revision

--- app/assets/scripts/vendor.js
+++ app/assets/scripts/vendor.js
@@ -1,1 +1,1 @@
-import particlesJs from "particles.js";/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */particlesJS.load('particles--bg', '../../assets/particles.json', function() {    });
+import particlesJs from "particles.js";/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */particlesJS.load('particles--bg', './assets/particles.json', function() {    });
