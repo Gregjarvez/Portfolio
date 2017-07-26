d30860ad8d6d573fc4f2919a51e09b1c16e4ba1b
GREGORY ASSASIE <gregjarvez@gmail.com>
2017-07-26 21:57:44 +0100

    project update

--- app/temp/scripts/app.js
+++ app/temp/scripts/app.js
@@ -9849,7 +9849,7 @@ window.onload = function () {
     toposition: "toposition",
     svgOffset: "30%",
     svg: ".skill--svg",
-    countObj: [{ countNode: ".count", stop: 41, delay: 200, speed: 50 }, { countNode: ".count--2", stop: 38, delay: 1050, speed: 50 }, { countNode: ".count--3", stop: 1, delay: 1900, speed: 50 }]
+    countObj: [{ countNode: ".count", stop: 21, delay: 200, speed: 50 }, { countNode: ".count--2", stop: 20, delay: 1050, speed: 50 }, { countNode: ".count--3", stop: 1, delay: 1900, speed: 50 }]
   };
 
   new _nav2.default();
